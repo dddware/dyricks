04d341de5e709974ae7bcdd4b0e8966c3684aa57
Victor Darras <victor.darras@gmail.com>
2017-04-24 23:54:50 +0200

    Implement genius/ug transition

--- public/dyrics.js
+++ public/dyrics.js
@@ -4,7 +4,7 @@
   var
     searchbar = document.getElementById('searchbar'),
     searchStyle = document.getElementById('searchStyle'),
-    songUg = document.getElementById('song-ug');
+    songUg = document.getElementById('song');
 
   function refreshSongList () {
     if (this.value.length > 0) {
@@ -14,28 +14,51 @@
     }
   }
 
+  window.toggleDataChords = function () {
+    if (document.body.hasAttribute('data-chords')) {
+      document.body.removeAttribute('data-chords');
+    } else {
+      document.body.setAttribute('data-chords', '');
+    }
+    queryService(document.querySelector('.toggle.active'));
+  }
+
   searchbar.addEventListener('keyup', refreshSongList);
 
+  function queryService(target) {
+    songUg.classList.remove('active');
+    if (target.getAttribute('data-clue')) {
+      songUg.classList.add('pending');
+    }
+
+    let dataSource =
+      document.body.hasAttribute('data-chords') ? 'ug' : 'genius';
+
+    // There's no local chords, let's fetch some on UG
+    fetch('/' + dataSource + '.html?q=' + target.getAttribute('data-clue').replace(/'/g, ''))
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(text) {
+        songUg.innerHTML = text;
+        songUg.classList.remove('pending');
+        songUg.classList.add('active');
+      })
+      .catch(console.error);
+  }
+
+  function toggleNav(target) {
+    if (document.querySelector('.toggle.active') !== null) {
+      document.querySelector('.toggle.active').classList.remove('active');
+    }
+    target.classList.add('active');
+  }
+
   Array.prototype.slice.call(document.querySelectorAll('.toggle'), 0).forEach(function(node) {
-    node.addEventListener('click', function() {
-      songUg.classList.remove('active');
-      if (this.getAttribute('data-clue')) {
-        songUg.classList.add('pending');
-      }
-
-      // There's no local chords, let's fetch some on UG
-      fetch('/ug.html?q=' + this.getAttribute('data-clue').replace(/'/g, ''))
-        .then(function(response) {
-          return response.text();
-        })
-        .then(function(text) {
-          songUg.innerHTML = text;
-          songUg.classList.remove('pending');
-          songUg.classList.add('active');
-        })
-        .catch(console.error);
-
-    });
+    node.addEventListener('click', function (e) {
+      queryService(e.currentTarget);
+      toggleNav(e.currentTarget);
+    } );
   });
 
   // Ambient light detection
